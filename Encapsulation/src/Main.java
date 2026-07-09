e2a22c997a95a884e2a566b692a52dfbbf15f913
Muhammad Sohaib Riaz <muhammadsohaibriaz19@gmail.com>
2026-07-09 14:27:08 +0500

    write getter and setter methods to get and set the values

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        Student s1 = new Student();
+        s1.setName("Sohaib");
+        s1.getName();
+        s1.setAge(22);
+        s1.getAge();
+        System.out.println("Name: "+s1.getName());
+        System.out.println("Age : "+s1.getAge());
+
+    }
+}
